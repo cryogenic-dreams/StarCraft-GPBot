9b6dd68cc0f255482ce94eae98b68ab7f5f8f96b
Cristina <cristina.corralm@gmail.com>
2017-04-10 15:29:24 +0200

    Update TechTree.java

--- GPBot/src/nonTerminal/TechTree.java
+++ GPBot/src/nonTerminal/TechTree.java
@@ -9,23 +9,23 @@ import ec.gp.GPIndividual;
 import ec.gp.GPNode;
 
 public class TechTree extends GPNode {
-/**	 * 	 */
 //OO gods forbid me
 private String name = "techtree";
 private int noChildren = 1;
 
 public String toString(){
   return this.name;// This is for the visual representation
-  }
+}
   
-	@Override
-  public String name() {
+@Override
+public String name() {
     return this.name;//this is for the magic
-  }
-	public int expectedChildren() {
+}
+
+public int expectedChildren() {
     return noChildren;//this
-  }
-	public void eval(final EvolutionState state, final int thread, final GPData input, final ADFStack stack, final GPIndividual individual, final Problem problem) {
-    GameData rd = ((GameData) (input));		//children[0].eval(state, thread, input, stack, individual, problem);
-	}
-  }
+}
+public void eval(final EvolutionState state, final int thread, final GPData input, final ADFStack stack, final GPIndividual individual, final Problem problem) {
+    GameData rd = ((GameData) (input));//children[0].eval(state, thread, input, stack, individual, problem);
+}
+}
