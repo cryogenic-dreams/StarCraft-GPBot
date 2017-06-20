e9de50d83b832eaeb49ce2fa641704cb7ad6ce41
Cristina <cristina.corralm@gmail.com>
2017-06-20 14:28:34 +0200

    Create Ifs.java

---
+++ GPBot/src/nonTerminal/Ifs.java
@@ -0,0 +1,33 @@
+package nonTerminal;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+
+public class Ifs extends GPNode {
+
+	private static final long serialVersionUID = 1;
+
+	public String toString() {
+		return "ifs";// This is for the visual
+	}
+
+	@Override
+	public String name() {
+		return "ifs";// this is for the magic
+	}
+
+	public int expectedChildren() {
+		return 2;// this
+	}
+
+	public void eval(final EvolutionState state, final int thread, final GPData input, final ADFStack stack,
+			final GPIndividual individual, final Problem problem) {
+
+		children[0].eval(state, thread, input, stack, individual, problem);
+		children[1].eval(state, thread, input, stack, individual, problem);
+	}
+}
