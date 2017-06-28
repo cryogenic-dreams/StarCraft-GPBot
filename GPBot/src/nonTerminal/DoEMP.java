3836cc4492481c2a9e4d4e7d544e1f3daca71ee1
Cristina <cristina.corralm@gmail.com>
2017-06-28 12:55:07 +0200

    Update DoEMP.java

--- GPBot/src/nonTerminal/DoEMP.java
+++ GPBot/src/nonTerminal/DoEMP.java
@@ -27,35 +27,45 @@ public class DoEMP extends GPNode {
 		return 2;
 	}
 
-	public void eval(final EvolutionState state, final int thread, final GPData input, final ADFStack stack,
+		public void eval(final EvolutionState state, final int thread, final GPData input, final ADFStack stack,
 			final GPIndividual individual, final Problem problem) {
 
 		//this does:
-			//Given a number of Vessel units, this node orders them to cast a shockwave to nearest enemy
+			//Given a number of Vessel units, this node orders them to cast a shockwave
 		GameData gd = ((GameData) (input));
 
-		children[0].eval(state, thread, input, stack, individual, problem);//quantity
-		children[1].eval(state, thread, input, stack, individual, problem);//enemies update
+		children[0].eval(state, thread, input, stack, individual, problem);
+		children[1].eval(state, thread, input, stack, individual, problem);
 		
 		int q = gd.q;
 		List<Unit> energy_enemies = null;
 		if(!gd.enemies.isEmpty()){
-			Unit enemy = null;
 			for (Unit e : gd.enemies){
+			//get enemies with energy
 				if(e.getEnergy() > 0){
 					energy_enemies.add(e);
 				}
 			}
 		}
-		if(!enemy.isNull()){
+
 		if (!gd.squads.isEmpty()) {
 			for (Unit unit : gd.squads) {
 				if ((unit.getType() == UnitType.Science_Vessel) && (!unit.isAttacking())) {
+					Unit enemy1 = energy_enemies.get(0);
+					int distance = unit.getDistance(energy_enemies.get(0));
+					for (Unit enemy2 : energy_enemies){
+						//get nearest enemy
+						if (unit.getDistance(enemy2) < distance){
+							distance = unit.getDistance(enemy2);
+							enemy1 = enemy2;
+						}
+					}
+					
 					if(TechType.Defensive_Matrix.targetsPosition()){
-						unit.useTech(TechType.EMP_Shockwave, enemy.getPosition());
+						unit.useTech(TechType.EMP_Shockwave, enemy1.getPosition());
 					}
 					else if (TechType.Defensive_Matrix.targetsUnit()){
-						unit.useTech(TechType.EMP_Shockwave, enemy);
+						unit.useTech(TechType.EMP_Shockwave, enemy1);
 					}
 					else unit.useTech(TechType.EMP_Shockwave);
 					q--;
@@ -64,7 +74,7 @@ public class DoEMP extends GPNode {
 					break;
 				}
 			}
-		}}
+		}
 
     }
 }
